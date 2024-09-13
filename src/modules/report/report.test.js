3bdedf28821582a8328cbcf4bd13b9fc212eec0e
haigaz15 <haigazsoghomonian@gmail.com>
2024-09-13 16:17:46 +0400

    adding unit tests

--- src/modules/report/report.test.js
+++ src/modules/report/report.test.js
@@ -1,1 +1,81 @@
-describe;
+const APIError = require("../../core/apiError");
+const ReportService = require("../../modules/report/report.service");
+const TaskRepository = require("../../repositories/task.repository");
+const UserRepository = require("../../repositories/user.repository");
+const UserTaskRepository = require("../../repositories/userTask.repository");
+
+const users = [
+  { _id: "asfsf", username: "user1", firstName: "user", lastName: "man" },
+];
+const task = {
+  _id: "sdsd",
+  title: "mock",
+  description: "mocking",
+  dueData: "2024-10-18",
+  priority: 1,
+  status: "completed",
+  progressLevel: 100,
+  createdAt: new Date("2024-09-13T06:17:02.377+00:00"),
+  completionDate: new Date("2024-09-13T06:22:32.936Z"),
+};
+
+const userTasks = {
+  _id: "sd",
+  userId: "asfsf",
+  taskId: "sdsd",
+};
+describe("Report Unit Test", () => {
+  test("Testing taskCompletionReport - 1 - ", async () => {
+    jest.spyOn(TaskRepository, "find").mockResolvedValue([task]);
+    jest.spyOn(UserTaskRepository, "find").mockResolvedValue([userTasks]);
+    jest.spyOn(UserRepository, "find").mockResolvedValue(users);
+    const result = await ReportService.taskCompletionReport(
+      {
+        query: {
+          completionDate: "1",
+          member: "1",
+        },
+      },
+      {}
+    );
+    expect(result[0].task).toBe("mock");
+    expect(new Date(result[0].completionDate).toString()).toBe(
+      new Date("2024-09-13T06:22:32.936Z").toString()
+    );
+  });
+
+  test("Testing taskCompletionReport - 2 - ", async () => {
+    try {
+      jest.spyOn(TaskRepository, "find").mockResolvedValue([]);
+      const result = await ReportService.taskCompletionReport(
+        {
+          query: {
+            completionDate: "1",
+            member: "1",
+          },
+        },
+        {}
+      );
+    } catch (err) {
+      if (err instanceof APIError)
+        expect(err.message).toBe("No tasks completed yet!");
+    }
+  });
+
+  test("Testing taskCompletionAvgTimeAndCount - 1 -", async () => {
+    try {
+      jest.spyOn(TaskRepository, "find").mockResolvedValue([]);
+      const result = await ReportService.taskCompletionAvgTimeAndCount({}, {});
+    } catch (err) {
+      if (err instanceof APIError)
+        expect(err.message).toBe("No tasks completed yet!");
+    }
+  });
+
+  test("Testing taskCompletionAvgTimeAndCount - 2 -", async () => {
+    jest.spyOn(TaskRepository, "find").mockResolvedValue([task]);
+    const result = await ReportService.taskCompletionAvgTimeAndCount({}, {});
+    expect(result.completedTasksCount).toBe(1);
+    expect(Math.ceil(result.averageTimeTookToComplete)).toBe(Math.ceil(0.09));
+  });
+});
--- src/modules/task/task.test.js
+++ src/modules/task/task.test.js
@@ -1,1 +1,101 @@
-describe("Task unit test suite: ", () => {});
+const TaskRepository = require("../../repositories/task.repository");
+const TaskService = require("../../modules/task/task.service");
+const UserRepository = require("../../repositories/user.repository");
+const UserTaskRepository = require("../../repositories/userTask.repository");
+const APIError = require("../../core/apiError");
+const users = [
+  { id: "asfsf", username: "user1", firstName: "user", lastName: "man" },
+];
+const task = {
+  id: "sdsd",
+  title: "mock",
+  description: "mocking",
+  dueData: "2024-10-18",
+  priority: 1,
+};
+const taskData = {
+  ...task,
+  assignedMembers: [{ username: "user1", firstName: "user", lastName: "man" }],
+};
+const userTasks = {
+  id: "sd",
+  userId: "asfsf",
+  taskId: "sdsd",
+};
+describe("Task unit test suite: ", () => {
+  test("Testing Create Task  - 1 - ", async () => {
+    try {
+      jest.spyOn(TaskRepository, "find").mockResolvedValue([task]);
+      await TaskService.createTask(
+        {
+          body: taskData,
+        },
+        { res: {} }
+      );
+    } catch (err) {
+      if (err instanceof APIError) {
+        expect(err.message).toBe("Task already exist!");
+      }
+    }
+  });
+
+  test("Testing Create Task - 2 - ", async () => {
+    jest.spyOn(UserRepository, "find").mockResolvedValue([]);
+    jest.spyOn(TaskRepository, "find").mockResolvedValue([]);
+    jest.spyOn(UserRepository, "insertManyUsers").mockResolvedValue([users]);
+    jest.spyOn(TaskRepository, "createTask").mockResolvedValue(task);
+    jest
+      .spyOn(UserTaskRepository, "insertManyUserTask")
+      .mockResolvedValue([userTasks]);
+    const tt = await TaskService.createTask(
+      {
+        body: taskData,
+      },
+      { res: {} }
+    );
+    expect(tt[0].taskId).toBe("sdsd");
+  });
+
+  test("Testing Get task details - 1 - ", async () => {
+    try {
+      jest.spyOn(TaskRepository, "findTaskById").mockResolvedValue(undefined);
+      await TaskService.getTaskDetails({ params: { taskId: "asf" } }, {});
+    } catch (err) {
+      if (err instanceof APIError)
+        expect(err.message).toBe("Task could not be found");
+    }
+  });
+  test("Testing Get task details - 2 - ", async () => {
+    jest.spyOn(TaskRepository, "findTaskById").mockResolvedValue(task);
+    const tt = await TaskService.getTaskDetails(
+      { params: { taskId: "sdsd" } },
+      {}
+    );
+    expect(tt.title).toBe("mock");
+  });
+
+  test("Testing update task details ", async () => {
+    jest.spyOn(TaskRepository, "findTaskById").mockResolvedValue(task);
+    jest.spyOn(UserRepository, "find").mockResolvedValue([]);
+    jest.spyOn(UserTaskRepository, "find").mockResolvedValue([userTasks]);
+    jest
+      .spyOn(UserTaskRepository, "updateUserTask")
+      .mockResolvedValue(undefined);
+    jest
+      .spyOn(TaskRepository, "updateTask")
+      .mockResolvedValue({ progressLevel: 5 });
+    const tt = await TaskService.updateTaskStatus(
+      {
+        params: { taskId: "sdsd" },
+        body: [
+          {
+            userId: "asfsf",
+            userContribution: 5,
+          },
+        ],
+      },
+      {}
+    );
+    expect(tt.progressLevel).toBe(5);
+  });
+});
